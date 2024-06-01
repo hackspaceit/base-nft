2281c29945e5eef3bcfc1cd59b816c241e61fec1
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-02 02:24:01 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,10 @@ const frameMetadata = getFrameMetadata({
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
+  title: 'By eldarfaaiz',
   description: 'LFG',
   openGraph: {
-    title: 'zizzamia.xyz',
+    title: 'By eldarfaaiz',
     description: 'LFG',
     images: [`${NEXT_PUBLIC_URL}/park-1.jpg`],
   },
