189ad919e93c2ac24c6aebd3b61d46a9d1d84c02
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-02 02:28:44 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
+      label: 'Check Eligibility',
     },
     {
       action: 'tx',
-      label: 'Send Base Sepolia',
+      label: 'Claim & Mint',
       target: `${NEXT_PUBLIC_URL}/api/tx`,
       postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     },
