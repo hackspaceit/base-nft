77aa6c90d741ef382a5eb6c71b0552b6620d745f
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-01 13:42:27 +0700

    commit

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'hhttps://ttp://localhost:3000' : 'https://zizzamia.xyz';
+  process.env.NODE_ENV == 'development' ? 'https://base-nft-git-main-hackspaceits-projects.vercel.app/';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
