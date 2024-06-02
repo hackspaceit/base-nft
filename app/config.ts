54709e78c5c1d8c7aae6e2ac2f1b4230ecd5df4f
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-02 20:46:44 +0700

    update

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL = 'https://base-nft-nine.vercel.app';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x7be85f86ebC27b64252f45ceedC26faE38D30C6f';
